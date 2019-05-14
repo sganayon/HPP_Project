71f5f85c6a3070769a34c3859994fa0a35bbd6be
Cliquetgos <Cliquetgos@Cliquetgo>
2019-05-14 16:20:12 +0200

    adding Reader class

---
+++ src/main/java/Reader/Reader.java
@@ -0,0 +1,40 @@
+package Reader;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import modeles.Post;
+
+
+public class Reader {
+	
+	
+	public String[] Reader() {
+		try{
+			InputStream flux=new FileInputStream(System.getProperty("user.home") + "\\Local Settings\\Application Data" + "/HPP_Project/posts.dat"); 
+			InputStreamReader lecture=new InputStreamReader(flux);
+			BufferedReader buff=new BufferedReader(lecture);
+			String ligne;			
+			ligne=buff.readLine();
+			System.out.println(ligne);
+			String[] mots = ligne.split("\\|");
+			for (int i=0;i<mots.length;i++) {
+				System.out.println(mots[i]);
+			}
+			buff.close();
+			return mots;
+			}					
+			catch (Exception e){
+			//System.out.println(e.toString());
+			return null;
+			}
+		
+	}
+	public static void toPost(String[] mots) {
+		
+		
+		
+	}
+}
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,10 +1,16 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	//Commenting to test
+		
+		
 	}
+	
 
 }
