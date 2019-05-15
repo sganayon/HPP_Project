fb65f61f7308721811d044024eb3e0cca0f310ab
POULAT Axel <axel.poulat1@gmail.com>
2019-05-15 14:26:47 +0200

    add data structure

---
+++ src/main/java/misc/Data.java
@@ -0,0 +1,34 @@
+package misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import modeles.Comments;
+import modeles.Post;
+
+public class Data {
+	private static List<Post> posts = new ArrayList<Post>();
+	
+	public static List<Post> getData(){
+		return posts;
+	}
+	
+	public static void addData(Object o) {
+		if(o instanceof Post) {
+			Post p = (Post) o;
+			posts.add(p);
+			posts.forEach(po->po.updateScore(p.getTime()));
+		}
+		else if(o instanceof Comments){
+			Comments c = (Comments) o;
+			for(Post p : posts) {
+				if(p.getId() == c.getPostId()) {
+					p.addComment(c);
+				}
+				p.updateScore(c.getTime());
+			}
+		}
+	}
+	
+	
+}
