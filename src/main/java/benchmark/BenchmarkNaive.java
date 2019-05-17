61729a6bd415dc371a7e53078b8d2aac02b7f76a
IlamaiTSE <clementcours@hotmail.fr>
2019-05-17 10:45:35 +0200

    Create class to make benchmarknaive usable

--- src/main/java/benchmark/BenchmarkNaive.java
+++ src/main/java/benchmark/BenchmarkNaive.java
@@ -17,43 +17,52 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
+import misc.BenchmarkPathing;
 import misc.Const;
 import reader.Reader;
 import writer.Output;
 
 @State(Scope.Benchmark)
 public class BenchmarkNaive {
-	
-	@Param({})
+
+	@Param({ BenchmarkPathing.Q1Basic, BenchmarkPathing.Q1Basic2, BenchmarkPathing.Q1BigTest, BenchmarkPathing.Q1Case1,
+			BenchmarkPathing.Q1Case2, BenchmarkPathing.Q1Case3, BenchmarkPathing.Q1Case4, BenchmarkPathing.Q1Case5,
+			BenchmarkPathing.Q1CommentCount, BenchmarkPathing.Q1PostExpiredComment,
+			BenchmarkPathing.Q1PostExpiredComment2 })
 	private String path;
-	
+
 	/**
-	 * the benchmark compute every operations that is in this method.
-	 * We are testing the iteration of an array.
-	 * @param Fork change the number of real test and warmup test 
-	 * @param Warmup change the number of iteration the warmup test make and the time an iteration last. 
-	 * @param Measurement same as Warmup but for real test
-	 * @param BenchmarkMode the test that are measured (here averageTime and throughput)
+	 * the benchmark compute every operations that is in this method. We are testing
+	 * the iteration of an array.
+	 * 
+	 * @param Fork          change the number of real test and warmup test
+	 * @param Warmup        change the number of iteration the warmup test make and
+	 *                      the time an iteration last.
+	 * @param Measurement   same as Warmup but for real test
+	 * @param BenchmarkMode the test that are measured (here averageTime and
+	 *                      throughput)
 	 * @return an int that ensure the compiler won't remove the code in this method
 	 */
-    @Benchmark
-    @Fork(value=1,warmups=2)
-    @Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-    @Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
-    @BenchmarkMode({Mode.Throughput, Mode.AverageTime})
-    public void testMethod() {
-    	Output.setFile(path);
+	@Benchmark
+	@Fork(value = 1, warmups = 2)
+	@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+	@Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+	@BenchmarkMode({ Mode.Throughput, Mode.AverageTime })
+	public void testMethod() {
+		Output.setFile(path);
 		Output.clearOutput();
-		
+
 		Reader.makeInput(path);
-    }
-    
-    /**
-     * initialize the list with random int
-     * @param Setup indicate that this method will be called before every test, it won't be included in the measurement
-     */
-    @Setup(Level.Invocation)
-    public void setUp() {
-    	
-    }
+	}
+
+	/**
+	 * initialize the list with random int
+	 * 
+	 * @param Setup indicate that this method will be called before every test, it
+	 *              won't be included in the measurement
+	 */
+	@Setup(Level.Invocation)
+	public void setUp() {
+
+	}
 }
