c3aa537f56abebe16f1546fd61433f4dfe01cdd7
POULAT Axel <axel.poulat1@gmail.com>
2019-05-22 10:11:10 +0200

    Add thrid optimisation, use Hashmap instead of list, reading time decrease from 40min to 10sec, computing time still slow

---
+++ src/main/java/misc/Data2.java
@@ -0,0 +1,234 @@
+package misc;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Vector;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import modeles.Comments;
+import modeles.Entree;
+import modeles.Post;
+import modeles.Top;
+
+public class Data2 {
+	//Id post - post
+	private static Map<Long,Post> posts = new HashMap<Long,Post>(500000);
+	//Id post - list de comments
+	private static Map<Long,List<Comments>> comments = new HashMap<Long,List<Comments>>(100000);
+	//Id comments - Id posts
+	private static Map<Long,Long> indexCP = new HashMap<Long,Long>(800000);
+	
+	private static List<Post> bestPosts = new ArrayList<Post>(3);
+	private static RWLock lock = new RWLock();
+	private static Vector<Long> oldData = new Vector<Long>();
+	private static Lock basicLock = new ReentrantLock();
+	
+	
+	public static void addPost(Post p) {
+		posts.put(p.getId(), p);
+	}
+	
+	public static long addComment(Comments c) {
+		//comment to a post
+		if(c.getPostId() != -1) {
+			List<Comments> lst = comments.get(c.getPostId());
+			basicLock.lock();
+			if(lst != null) {
+				lst.add(c);
+			}else {
+				lst = new ArrayList<Comments>();
+				lst.add(c);
+				comments.put(c.getPostId(),lst);
+			}
+			basicLock.unlock();
+			indexCP.put(c.getId(),c.getPostId());
+			return c.getPostId();
+		}else { //comment to a comment 
+			List<Comments> lst = comments.get(indexCP.get(c.getRepId()));
+			basicLock.lock();
+			lst.add(c);
+			basicLock.unlock();
+			indexCP.put(c.getId(),lst.get(0).getPostId());
+			return lst.get(0).getPostId();
+		}
+		
+	}
+
+	public static int getScoreOfPostAt(Post p, Timestamp t) {
+		int scoreComm = 0;
+		List<Comments> lst = comments.get(p.getId());
+		if(lst != null) {
+			basicLock.lock();
+			for(Comments c :lst) {
+				scoreComm+= c.getScoreAt(t);
+			}
+			basicLock.unlock();
+		}
+		
+		int scoreTot = p.getScoreAt(t)+scoreComm;
+		
+		return scoreTot;
+	}
+	
+	public static List<Post> getTop3At(Timestamp t) {
+		List<Post> best3 = new ArrayList<Post>(3);
+		
+		removeOldData();
+		
+		for(int i=0;i<3;i++) {
+			best3.add(null);
+		}
+		
+		lock.readLock();
+		for(Entry<Long, Post> entry : posts.entrySet()) {
+			Post p = entry.getValue();
+			if(!p.getTime().after(t)) {
+				int scoreOfP = getScoreOfPostAt(p,t);
+				if(scoreOfP != 0) {
+					if(best3.get(0) == null || scoreOfP > best3.get(0).getScore() || (scoreOfP == best3.get(0).getScore() && p.getTime().after(best3.get(0).getTime()))) {
+						
+						best3.set(2,best3.get(1));
+						best3.set(1,best3.get(0));
+						Post p0 =p.clone();
+						p0.setScore(scoreOfP);
+						p0.setNbComm(getNbCommofPostAt(p,t));
+						best3.set(0,p0);
+					
+					}else if(best3.get(1) == null || scoreOfP > best3.get(1).getScore() || (scoreOfP == best3.get(1).getScore() && p.getTime().after(best3.get(1).getTime()))) {
+					
+						best3.set(2,best3.get(1));
+						Post p0 =p.clone();
+						p0.setScore(scoreOfP);
+						p0.setNbComm(getNbCommofPostAt(p,t));
+						best3.set(1,p0);
+					
+					}else if (best3.get(2) == null || scoreOfP > best3.get(2).getScore() || (scoreOfP == best3.get(2).getScore() && p.getTime().after(best3.get(2).getTime()))) {
+					
+						Post p0 =p.clone();
+						p0.setScore(scoreOfP);
+						p0.setNbComm(getNbCommofPostAt(p,t));
+						best3.set(2,p0);
+					
+					}
+				}else {
+					oldData.add(p.getId());
+				}
+			}
+		}
+		lock.readUnLock();
+		return best3;
+	}
+
+	public static List<Post> getTop3AtNotMultiThreaded(Entree e){
+		List<Post> lst = new ArrayList<Post>(3);
+		
+		Post p = posts.get(e.getId());
+		if(p == null) {
+			for(int i=0; i<3;i++) {
+				if(bestPosts.get(i) == null) {
+					lst.add(null);
+				}else {
+					lst.add(bestPosts.get(i).clone());
+				}
+			}
+			return lst;
+		}
+		int scoreOfP = getScoreOfPostAt(p, e.getTime());
+		boolean isAlreadyIn = false;
+		for(int i=0;i<3;i++) {
+			if(bestPosts.get(i) != null) {
+				bestPosts.get(i).setScore(getScoreOfPostAt(bestPosts.get(i),e.getTime()));
+				bestPosts.get(i).setNbComm(getNbCommofPostAt(bestPosts.get(i),e.getTime()));
+				if(bestPosts.get(i).getId() == p.getId()) {
+					isAlreadyIn = true;
+				}
+			}
+		}
+		
+		if(!isAlreadyIn) {
+			if(bestPosts.get(0) == null || scoreOfP >= bestPosts.get(0).getScore()) {
+				bestPosts.set(2,bestPosts.get(1));
+				bestPosts.set(1,bestPosts.get(0));
+				Post p0 =p.clone();
+				p0.setScore(scoreOfP);
+				p0.setNbComm(getNbCommofPostAt(p,e.getTime()));
+				bestPosts.set(0,p0);
+				
+			}else if(bestPosts.get(1) == null || scoreOfP >= bestPosts.get(1).getScore()) {
+				bestPosts.set(2,bestPosts.get(1));
+				Post p0 =p.clone();
+				p0.setScore(scoreOfP);
+				p0.setNbComm(getNbCommofPostAt(p,e.getTime()));
+				bestPosts.set(1,p0);
+				
+			}else if(bestPosts.get(2) == null || scoreOfP >= bestPosts.get(2).getScore()) {
+				Post p0 =p.clone();
+				p0.setScore(scoreOfP);
+				p0.setNbComm(getNbCommofPostAt(p,e.getTime()));
+				bestPosts.set(2,p0);
+				
+			}
+		}
+		
+		for(int i=0; i<3;i++) {
+			if(bestPosts.get(i) == null) {
+				bestPosts.set(i,new Post(null,-1,"uer",-i-1,new ArrayList<Comments>()));
+			}
+		}
+		Collections.sort(bestPosts);
+		Collections.reverse(bestPosts);
+		for(int i=0; i<3;i++) {
+			if(bestPosts.get(i).getId() == -1) {
+				bestPosts.set(i,null);
+				lst.add(null);
+			}else {
+				lst.add(bestPosts.get(i).clone());
+			}
+		}
+		return lst;
+	}
+	
+	public static void removeOldData() {
+		if(oldData.size() > 10) {
+			lock.writeLock();
+			for(Long id : oldData) {
+				posts.remove(id);
+				comments.remove(id);
+			}
+			oldData.clear();
+			lock.writeUnLock();
+			System.out.println("clearing 10 tops");
+		}
+	}
+	
+	public static int getNbCommofPostAt(Post p, Timestamp t) {
+		List<Comments> lst = comments.get(p.getId());
+		if(lst != null){
+			basicLock.lock();
+			int nbComm = (int) lst.stream().filter(c->!c.getTime().after(t)).mapToLong(c->c.getUserId()).distinct().count();
+			basicLock.unlock();
+			return nbComm;
+		}else {
+			return 0;
+		}
+		
+	}
+
+	public static void clearData() {
+		posts.clear();
+		comments.clear();
+		indexCP.clear();
+		oldData.clear();
+		bestPosts.clear();
+		bestPosts.add(null);
+		bestPosts.add(null);
+		bestPosts.add(null);
+		
+	}
+}
---
+++ src/main/java/reader/Reader2.java
@@ -0,0 +1,212 @@
+package reader;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import modeles.Comments;
+import modeles.Entree;
+import misc.Data;
+import misc.Data2;
+import misc.TurnInto;
+import modeles.Comments;
+import modeles.Post;
+
+public class Reader2 {
+	private static List<Timestamp> timeLst=null;
+	private static InputStream fluxPosts = null;
+	private static InputStream fluxComments = null;
+	private static InputStreamReader ReaderPosts = null;
+	private static InputStreamReader ReaderComments = null;
+	private static BufferedReader buffPosts = null;
+	private static BufferedReader buffComments = null;
+
+	public Reader2() {
+
+	}
+
+	/**
+	 * read a ligne from a bufferedReader
+	 * @param buff
+	 * @return the ligne or null if there are no more lignes
+	 */
+	public static String readBuff(BufferedReader buff) {
+		String ligne = null;
+		try {
+			ligne = buff.readLine();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return ligne;
+	}
+
+	/**
+	 * open all flux, create streamReader, bufferedRead
+	 * @param Path the path to the files
+	 */
+	public static void openFlux(String Path) {
+		try {
+			fluxPosts = new FileInputStream(Path+"posts.dat");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			fluxComments = new FileInputStream(Path+"comments.dat");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ReaderPosts = new InputStreamReader(fluxPosts);
+		buffPosts = new BufferedReader(ReaderPosts);
+
+		ReaderComments = new InputStreamReader(fluxComments);
+		buffComments = new BufferedReader(ReaderComments);
+	}
+
+	/**
+	 * close all flux, streamReader, BufferedReader
+	 */
+	public static void closeFlux() {
+		try {
+			fluxPosts.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			fluxComments.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			ReaderPosts.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			ReaderComments.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			buffPosts.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			buffComments.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * read the files and add the data into Data class
+	 * @param Path the path to the files
+	 * 
+	 */
+
+	public static void makeInput(String Path, Vector<Timestamp> timeLst){
+		//first open and create all things we need
+		openFlux(Path);
+
+		// read the first line of the files
+		String lignePost = readBuff(buffPosts);
+		String ligneComment = readBuff(buffComments);
+
+		// while they are not null and not empty
+		while (lignePost != null && ligneComment != null && !lignePost.isEmpty() && !ligneComment.isEmpty()) {
+			// split the data
+			String[] motsPosts = lignePost.split("\\|");
+			String[] motsComments = ligneComment.split("\\|");
+
+			//compare the timestamp and add the older to the Data class, then read the next line
+			if (TurnInto.timeStamp(motsPosts[0]).after(TurnInto.timeStamp(motsComments[0]))) {
+				
+				Comments C = toComment(motsComments);
+				// M�thode pour envoyer le commentaire dans la chaine principale
+				Data2.addComment(C);
+				timeLst.add(C.getTime());
+				System.out.println(C.getTime());
+				ligneComment = readBuff(buffComments);		
+			}
+			else {
+				Post P = toPost(motsPosts);
+				// M�thode pour envoyer le post dans la chaine principale
+				Data2.addPost(P);
+				timeLst.add(P.getTime());
+				System.out.println(P.getTime());
+				lignePost = readBuff(buffPosts);	
+			}
+		}
+
+		// one of the file is completely read, finish the reading on the other file
+		if (lignePost == null) {
+			while (ligneComment != null) {
+				Comments C = toComment(ligneComment.split("\\|"));
+				// M�thode pour envoyer le commentaire dans la chaine principale
+				Data2.addComment(C);
+				timeLst.add(C.getTime());
+				System.out.println(C.getTime());
+				ligneComment = readBuff(buffComments);
+			}
+		} else {
+			while (lignePost != null) {
+				Post P = toPost(lignePost.split("\\|"));
+				// M�thode pour envoyer le post dans la chaine principale
+				Data2.addPost(P);
+				System.out.println(P.getTime());
+				timeLst.add(P.getTime());
+				lignePost = readBuff(buffPosts);
+			}
+		}
+
+		// close everything
+		closeFlux();
+		System.out.println("Done Reading");
+	}
+
+	/**
+	 * turn several strings into one class
+	 * @param mots the splited line of a comment
+	 * @return class that keep the data
+	 */
+	public static Comments toComment(String[] mots) {
+		Timestamp t = TurnInto.timeStamp(mots[0]);
+		long repId = (mots[5].isEmpty()) ? -1 : Long.valueOf(mots[5]);
+		long postId = (mots.length == 6) ? -1 : Long.valueOf(mots[6]);
+		return new Comments(t, Long.valueOf(mots[1]), Long.valueOf(mots[2]), repId, postId);
+	}
+
+	/**
+	 * turn several strings into one class
+	 * @param mots the splited line of a post
+	 * @return class that keep the data
+	 */
+	public static Post toPost(String[] mots) {
+		Timestamp t = TurnInto.timeStamp(mots[0]);
+		return new Post(t, Long.valueOf(mots[1]), mots[4]);
+	}
+}
